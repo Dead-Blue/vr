62d7942c380f880cff1d3e2656c8c2ffeefcaa1c
DeadBlue <406354451@qq.com>
2018-03-28 12:25:20 +0800

    modify

--- index.vr.js
+++ index.vr.js
@@ -11,7 +11,7 @@ export default class WelcomeToVR extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
+        <Pano source={asset('TEST2.jpg')}/>
         {/* <Text
           style={{
             backgroundColor: '#777879',
